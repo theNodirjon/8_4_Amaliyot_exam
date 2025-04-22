26262b8cc12d76edad040abbc52b7ede69c44c9a
Nodir <116571203+theNodirjon@users.noreply.github.com>
2025-04-22 23:28:33 +0500

    serverda migrate uchun

--- configapp/admin.py
+++ configapp/admin.py
@@ -1,5 +1,4 @@
-
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register([Teacher,Departments,Course,User,Student,GroupStudent])
+admin.site.register([Teacher,Departments,Course,User,Student,GroupStudent,Attendance])
