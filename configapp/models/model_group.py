5f0b5f8c3e94974b39469a53ce2001c22a373a91
Nodir <116571203+theNodirjon@users.noreply.github.com>
2025-05-01 22:35:51 +0500

    group-teacher qo'shmaydi

--- configapp/models/model_group.py
+++ configapp/models/model_group.py
@@ -5,7 +5,7 @@ from .model_teacher import Teacher, Course
 class GroupStudent(BaseModel):
     title = models.CharField(max_length=40, unique=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    teacher = models.ManyToManyField(Teacher, related_name='teacher_get')
+    # teacher = models.ManyToManyField(Teacher, related_name='teacher_get')
 
     def __str__(self):
-        return  f"{self.title} - {self.course} - {self.teacher}"
+        return  f"{self.title} - {self.course}"
