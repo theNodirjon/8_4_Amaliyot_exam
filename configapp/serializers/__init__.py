63b9c611cca3d53a89d2e34dc7fdda28cb5d8217
Nodir <116571203+theNodirjon@users.noreply.github.com>
2025-05-04 16:33:08 +0500

    yangilandi

--- configapp/serializers/__init__.py
+++ configapp/serializers/__init__.py
@@ -1,5 +1,5 @@
 from .login_serializer import *
-# from .student_serializer import *
+from .student_serializer import *
 from .teacher_serializer import *
 from .group_serializer import *
 from .attendance_serializer import *
